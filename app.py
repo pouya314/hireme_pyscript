1a07368d429cdc2eda2dd34989b070dfbe73698c
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2024-07-18 15:13:07 +1000

    add eligibility and application page and setup pyscript

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def hireme():
     return render_template('index.html')
 
 
+@app.route('/eligibility_and_application')
+def eligibility_and_application():
+    return render_template('eligibility_and_application.html')
+
+
 @app.route('/up')
 def up():
     return 'ok'
