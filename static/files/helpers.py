b0a30cc6cfa0898bd2e7f1ed6da58f3ed0463de3
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-02-14 17:27:10 +1100

    add new helpers

--- static/files/helpers.py
+++ static/files/helpers.py
@@ -8,3 +8,30 @@ def get_root_url():
 
 def get_data(filename):
     return requests.get(f"{get_root_url()}static/data/{filename}").json()
+
+
+def determine_current_question(questions):
+    if any(question_has_acceptance_errors(question) for question in questions):
+        return None
+
+    return next((question for question in questions if not_answered(question)), None)
+
+
+def question_has_acceptance_errors(question):
+    return question["acceptance_errors"] != []
+
+
+def not_answered(question):
+    return question_encountered_first_time(question) or question_has_validation_errors(question)
+
+
+def question_encountered_first_time(question):
+    return (question["provided_answer"] is None and 
+            question["validation_errors"] == [] and 
+            question["acceptance_errors"] == [])
+
+
+def question_has_validation_errors(question):
+    return (question["provided_answer"] is not None and 
+            question["validation_errors"] != [] and 
+            question["acceptance_errors"] == [])
