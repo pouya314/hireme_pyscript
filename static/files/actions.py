0b950414af0a57bb050cc62072f70faf0a603c51
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-02-14 17:28:11 +1100

    Refactor state initialization and question handling in wizard

--- static/files/actions.py
+++ static/files/actions.py
@@ -1,25 +1,48 @@
 from typing import List, Dict
 
-from toolz import assoc_in
+from toolz import assoc_in, concat
 
 from .helpers import get_data
+from .constants import QUESTION_CATEGORY_ELIGIBILITY, QUESTION_CATEGORY_APPLICATION
 
 
 def initialize_action(state, action):
     print("performing `initialize_action()` on state: {}".format(state))
 
-    # Read and sort the data
-    sorted_eligibility_questions: List[Dict] = sorted(get_data("questions.json"), key=lambda question: question['position'])
-    sorted_application_questions: List[Dict] = sorted(get_data("application.json"), key=lambda question: question['position'])
+    eligibility_questions: List[Dict] = [
+        assoc_in(question, ["category"], QUESTION_CATEGORY_ELIGIBILITY) 
+        for question in get_data("questions.json")
+    ]
+    application_questions: List[Dict] = [
+        assoc_in(question, ["category"], QUESTION_CATEGORY_APPLICATION) 
+        for question in get_data("application.json")
+    ]
 
-    # Prepare the state for initial load
-    state_with_sorted_eligibility_qs = assoc_in(state, ["eligibility_questions"], sorted_eligibility_questions)
-    state_with_sorted_application_qs = assoc_in(state_with_sorted_eligibility_qs, ["application_questions"], sorted_application_questions)
-    state_with_current_q = assoc_in(state_with_sorted_application_qs, ["current_question"], sorted_eligibility_questions[0])
+    all_questions = tuple(concat([eligibility_questions, application_questions]))
 
-    return state_with_current_q
+    for question in all_questions:
+        question["provided_answer"] = None
+        question["validation_errors"] = []
+        question["acceptance_errors"] = []
+
+    state_with_questions_populated = assoc_in(state, ["questions"], all_questions)
+
+    return state_with_questions_populated
 
 
 def submit_action(state, action):
-    print("performing `submit_action()` on state: {}".format(state))
+    print("performing `submit_action()` on state: {}, and action: {}".format(state, action))
+    action_data = action["data"]
+    question_uuid = action_data["uuid"]
+    provided_answer = action_data["provided_answer"]
+
+    the_question = next((question for question in state["questions"] if question["uuid"] == question_uuid), None)
+    if the_question is None:
+        print("Question not found in eligibility or application questions")
+        return state
+
+    the_question["provided_answer"] = provided_answer
+
+    # TODO: validate first, then check acceptance criteria
+
     return state
