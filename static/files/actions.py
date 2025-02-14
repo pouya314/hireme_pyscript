0a0c92bafabdd2d13ddc7fc670eb9116cf1167f3
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-02-14 14:04:03 +1100

    implement initialize action

--- static/files/actions.py
+++ static/files/actions.py
@@ -7,15 +7,17 @@ from .helpers import get_data
 
 def initialize_action(state, action):
     print("performing `initialize_action()` on state: {}".format(state))
-    eligibility_questions: List[Dict] = get_data("questions.json")
-    application_questions: List[Dict] = get_data("application.json")
-    # sorted(eligibility_questions, key=lambda question: question['position'])
-    state_with_eligibility_qs = assoc_in(
-        state, ["eligibility_questions"], eligibility_questions
-    )
-    return assoc_in(
-        state_with_eligibility_qs, ["application_questions"], application_questions
-    )
+
+    # Read and sort the data
+    sorted_eligibility_questions: List[Dict] = sorted(get_data("questions.json"), key=lambda question: question['position'])
+    sorted_application_questions: List[Dict] = sorted(get_data("application.json"), key=lambda question: question['position'])
+
+    # Prepare the state for initial load
+    state_with_sorted_eligibility_qs = assoc_in(state, ["eligibility_questions"], sorted_eligibility_questions)
+    state_with_sorted_application_qs = assoc_in(state_with_sorted_eligibility_qs, ["application_questions"], sorted_application_questions)
+    state_with_current_q = assoc_in(state_with_sorted_application_qs, ["current_question"], sorted_eligibility_questions[0])
+
+    return state_with_current_q
 
 
 def submit_action(state, action):
