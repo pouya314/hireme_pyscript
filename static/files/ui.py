909d07b8dfb7ce23dbd3cad3d0d11b5a6eb1e35d
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-02-14 17:27:55 +1100

    Pass helpers module to Jinja2 template rendering

--- static/files/ui.py
+++ static/files/ui.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, BaseLoader
 from pyscript import document
+from . import helpers
 
 
 def render_ui(state):
@@ -8,7 +9,7 @@ def render_ui(state):
     wizard_template_string = document.getElementById("wizard-template").innerHTML
 
     rtemplate = Environment(loader=BaseLoader()).from_string(wizard_template_string)
-    rendered_html = rtemplate.render(**state)
+    rendered_html = rtemplate.render(helpers=helpers, **state)
 
     wizard_container = document.getElementById("wizard-container")
     wizard_container.innerHTML = rendered_html
