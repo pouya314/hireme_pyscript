273b7f968e15bb3b0bdc1a8d1e3726557a1e378f
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-05-16 11:17:28 +1000

    Add deployment scripts for production and staging
    Introduce `deploy_to_production.sh` and `deploy_to_staging.sh` to handle environment variable exports, secrets printing, and deployment execution. These scripts streamline the deployment process for both environments.

---
+++ deploy_to_staging.sh
@@ -0,0 +1,7 @@
+echo "Exporting env vars into the shell environment"
+export $(grep -v '^#' .env.staging | xargs)
+
+echo "Printing Kamal Secrets on the screen"
+kamal secrets print
+
+echo "Deploying to Staging"
