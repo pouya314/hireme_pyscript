2051260f56e88aa22360ea4a68370e3f2531d49f
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2025-06-20 10:58:21 +1000

    Comment out printing of Kamal Secrets in deployment scripts for security reasons

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -1,8 +1,8 @@
 echo "Exporting env vars into the shell environment"
 export $(grep -v '^#' .env.production | xargs)
 
-echo "Printing Kamal Secrets on the screen"
-kamal secrets print
+# echo "Printing Kamal Secrets on the screen"
+# kamal secrets print
 
 echo "Deploying to Production"
 #kamal setup -d production
--- deploy_to_staging.sh
+++ deploy_to_staging.sh
@@ -1,8 +1,8 @@
 echo "Exporting env vars into the shell environment"
 export $(grep -v '^#' .env.staging | xargs)
 
-echo "Printing Kamal Secrets on the screen"
-kamal secrets print
+# echo "Printing Kamal Secrets on the screen"
+# kamal secrets print
 
 echo "Deploying to Staging"
 #kamal setup -d staging
