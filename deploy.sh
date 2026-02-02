c1234e2f9644cfd5ebb050e177f9f6a26949778c
Pouya Arvandian <pouya.arvandian@gpt.com.au>
2026-02-02 16:18:50 +1100

    improve the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Select environment:"
 echo "1) Staging"
 echo "2) Production"
@@ -6,9 +8,11 @@ read -p "Enter choice [1 or 2]: " choice
 case $choice in
     1)
         ENVIRONMENT="staging"
+        DISPLAY_NAME="Staging"
         ;;
     2)
         ENVIRONMENT="production"
+        DISPLAY_NAME="Production"
         ;;
     *)
         echo "Invalid choice. Exiting."
@@ -22,6 +26,6 @@ export $(grep -v '^#' .env.$ENVIRONMENT | xargs)
 # echo "Printing Kamal Secrets on the screen"
 # kamal secrets print
 
-echo "Deploying to ${ENVIRONMENT^}"
+echo "Deploying to $DISPLAY_NAME"
 #kamal setup -d $ENVIRONMENT
 kamal deploy -d $ENVIRONMENT
